8ba316ed68991e433056b7bbeb2a016e404f4f0c
Ram teja <sivaram@codegnan.com>
2022-03-07 13:11:27 -0800

    first commit

---
+++ req. body.js
@@ -0,0 +1,76 @@
+const cors =require('cors');
+const express=require('express');
+const mysql =require('mysql');
+const bodyParser= require('body-parser')
+
+const app=express();
+
+app.use(bodyParser.urlencoded({
+	extended:true
+}))
+
+
+app.use(bodyParser.json())
+
+
+const SELECT='SELECT * FROM student';
+const connection =mysql.createConnection({
+
+	host:'localhost',
+	user:'root',
+	password:'root',
+	database:'first_db'
+});
+
+connection.connect(err=>{
+	if(err)
+		return err;
+});
+
+app.use(cors());
+
+app.get('/',(req,res)=>{
+	res.send('hello this is from server');
+});
+
+app.post('/student/add',(req,res)=>{
+	console.log(req);
+	const {name}=req.body;
+	const insert=`INSERT INTO student(name) VALUE('${name}')`;
+	connection.query(insert,(err,results)=>{
+		if(err)
+			return res.send(err);
+		else
+			return res.send('Data is added successfully');
+	})
+});
+
+app.get('/student',(req,res)=>{
+	connection.query(SELECT,(err,results)=>{
+		if(err)
+			return res.send(err)
+		else{
+			return res.json({
+				data:results
+			})
+		}
+	});
+});
+
+
+app.listen(5000,()=>{
+	console.log(`Listening on port 5000`)
+})
+
+//front end
+function pos(){
+			//var data={name:"teja"}
+			fetch("http://localhost:5000/student/add",{
+				method:"POST",
+				headers:{
+					'Content-Type':'application/json'
+				},
+				body:JSON.stringify({name:"killer"})
+			}).then((res)=>console.log(res))
+			.catch((err)=>console.log(err))
+		}
